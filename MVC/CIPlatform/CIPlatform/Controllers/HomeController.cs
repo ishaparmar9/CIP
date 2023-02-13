bd4677cf96acacf0252032876d1f4cfbdec9dbdc
ishaparmar9 <ishahparmar2000@gmail.com.com>
2023-02-13 18:26:14 +0530

    MVC files Added

---
+++ MVC/CIPlatform/CIPlatform/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using CIPlatform.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CIPlatform.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Lost()
+        {
+            return View();
+        }
+
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+
+        public IActionResult Reset()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
