89853fb50bf814bea8bed2cb73df11dca87717b2
ishaparmar9 <ishahparmar2000@gmail.com.com>
2023-02-21 17:19:16 +0530

    Designed Volunteering Mission page

--- MVC/CIPlatform/CIPlatform/Controllers/HomeController.cs
+++ MVC/CIPlatform/CIPlatform/Controllers/HomeController.cs
@@ -50,6 +50,11 @@ namespace CIPlatform.Controllers
             return View();
         }
 
+        public IActionResult VolunteeringMission()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
